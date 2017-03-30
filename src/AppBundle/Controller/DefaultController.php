6ddca28f482f71742999080fd41de59d81e4e43b
Maja Kędras <r2d2ka@gmail.com>
2017-03-30 07:50:47 +0200

    User list and profiles

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class DefaultController extends Controller
 {
@@ -18,4 +21,61 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
+
+
+    /**
+     * @Route("/new/user")
+     */
+
+    public function addUser()
+    {
+        $user = new User();
+        $user->setLogin('listek');
+        $user->setPassword('haslo_listka');
+        $user->setEmail('listek@mail.com');
+
+        $em = $this->getDoctrine()->getManager();
+
+        // tells Doctrine you want to (eventually) save the Product (no queries yet)
+        $em->persist($user);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $em->flush();
+
+        return new Response('Dodano nowego użytkownika o ID '.$user->getId());
+    }
+
+    /**
+     * @Route("/show/users")
+     */
+
+    public function showUsers()
+    {
+       $repository = $this->getDoctrine()->getRepository('AppBundle:User');
+       $users = $repository->findAll(); 
+      
+       return $this->render('show/users.html.twig', array(
+           'users' => $users,
+       ));
+ 
+    }
+
+    /**
+     * @Route("/show/user/{user_id}", requirements={"page": "\d+"})
+     */
+
+    public function showUser(Request $request)
+    {
+       $repository = $this->getDoctrine()->getRepository('AppBundle:User');
+       //$request = $this->getRequest();
+       $user_id = $request->attributes->get('user_id');
+       $user = $repository->findOneById($user_id);
+      
+       return $this->render('show/user.html.twig', array(
+           'user' => $user,
+       ));
+ 
+    }
+
+
 }
