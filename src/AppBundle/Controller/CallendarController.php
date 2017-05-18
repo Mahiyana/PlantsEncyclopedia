e1178a7bc0ebeeff01b808e76b6b84670978dd0e
Maja Kędras <r2d2ka@gmail.com>
2017-05-18 16:16:51 +0200

    Added event entity and created simple adding panel for events

--- src/AppBundle/Controller/CallendarController.php
+++ src/AppBundle/Controller/CallendarController.php
@@ -11,6 +11,7 @@ use AppBundle\Entity\Image;
 use AppBundle\Entity\Role;
 use AppBundle\Entity\Category;
 use AppBundle\Entity\Article;
+use AppBundle\Entity\Event;
 use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -18,6 +19,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 use Ivory\CKEditorBundle\Form\Type\CKEditorType;
 
@@ -31,6 +34,54 @@ class CallendarController extends Controller
      */
     public function showCallendar(Request $request)
     {
+       $repository = $this->getDoctrine()->getRepository('AppBundle:Event');
+       $events = $repository->findAll(); 
+
+       return $this->render('show/callendar.html.twig', array(
+           'events' => $events,
+       ));
+
+        
         return $this->render('show/callendar.html.twig');
     }
+
+    /**
+     * @Route("/add/event")
+     */
+    public function addEvent(Request $request)
+    {
+     
+       $event = new Event();
+       $form = $this->createFormBuilder($event)
+            ->add('name', TextType::class, array('label' => $this->get('translator')->trans('Nazwa')))
+            ->add('date', DateTimeType::class, array('label' => $this->get('translator')->trans('Data') ))
+            ->add('anniversary', CheckboxType::class, array(
+              'label' => $this->get('translator')->trans('Rocznica'),
+              'required' => false,
+              ))
+            ->add('save', SubmitType::class, array('label' => $this->get('translator')->trans('Dodaj wydarzenie')))
+            ->getForm();
+       
+   
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+        $event = $form->getData();
+        
+        $event->setAuthor($this->container->get('security.token_storage')->getToken()->getUser());
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($event);
+        $em->flush();
+
+        return $this->redirect('/show/callendar' );
+        
+        }
+        return $this->render('add/event.html.twig', array(
+            'form' => $form->createView(),
+        ));
+
+
+   }
+
+
 }
---
+++ src/AppBundle/Entity/Event.php
@@ -0,0 +1,152 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="`event`")
+*/
+
+class Event
+{
+    
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="images")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    private $author;
+
+    /**
+     * @Assert\NotBlank()
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+     /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $anniversary;
+
+   
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Event
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Event
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set anniversary
+     *
+     * @param boolean $anniversary
+     *
+     * @return Event
+     */
+    public function setAnniversary($anniversary)
+    {
+        $this->anniversary = $anniversary;
+
+        return $this;
+    }
+
+    /**
+     * Get anniversary
+     *
+     * @return boolean
+     */
+    public function getAnniversary()
+    {
+        return $this->anniversary;
+    }
+
+    /**
+     * Set author
+     *
+     * @param \AppBundle\Entity\User $author
+     *
+     * @return Event
+     */
+    public function setAuthor(\AppBundle\Entity\User $author = null)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return \AppBundle\Entity\User
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+}
